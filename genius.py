b321cdb71217b76b570bd3a726b48341e9eea1dc
Janujan <janujan.selva@gmail.com>
2019-01-01 23:12:25 -0500

    removing client details

--- genius.py
+++ genius.py
@@ -2,10 +2,6 @@
 import lyricsgenius as genius
 import json
 
-client_id = "QDLq4h4n98Jlq9AoFOSqbW6ESGVCId5j4HDCZ3fkrNiFu9s_R89PjhojrqrB_IOi"
-
-client_secret = "mwiQX_N4dgd813ulAJJE4_lhRSCpWM0cM6APJgM03pZHx84NdVz5F4bpTVOuSp3TfZpma4cTyw2ZpMzNl3_1Sg"
-
 access_token ="H8gi1tgFffzZjO9PRxMNly8l04APCJHQNodtzCRGs_bIweR0x5JvhT7h6dq1-kED"
 
 api = genius.Genius(access_token)
