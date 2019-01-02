37fc7d7dcf3c72c78f1fb914ca9e12790b5b1ec2
Janujan <janujan.selva@gmail.com>
2019-01-01 23:07:49 -0500

    added info on how to use the function calls

--- genius.py
+++ genius.py
@@ -9,15 +9,31 @@ client_secret = "mwiQX_N4dgd813ulAJJE4_lhRSCpWM0cM6APJgM03pZHx84NdVz5F4bpTVOuSp3
 access_token ="H8gi1tgFffzZjO9PRxMNly8l04APCJHQNodtzCRGs_bIweR0x5JvhT7h6dq1-kED"
 
 api = genius.Genius(access_token)
-artist = api.search_artist("Drake", max_songs=1)
-#print(artist.songs)
+artist = api.search_artist("Drake", max_songs=3, sort='popularity', get_full_info=False)
+
+#get_full_info to false speeds up search considerably, you drop annotations
+# can sort on popularity or title
+# Max songs to none is all songs
 
 #save_lyrics saves a json file unfortunately, need to find a way to stop that
 #OR just delete after each run
-data = artist.save_lyrics()
+data = artist.save_lyrics(overwrite=False)
 
 #json tags of d:
 # songs, title, album, year, lyrics
 # after that its just a bunch of annotations
 extract = data['songs'][0]['year']
 print(extract)
+
+
+extract = data['songs'][0]['album']
+print(extract)
+
+extract = data['songs'][0]['title']
+print(extract)
+
+extract = data['songs'][0]['lyrics']
+print(extract)
+
+artist_name = data['artist']
+print(artist_name)
